b42b5abf0c3de90f9a95da534ca62c2f6d15cd79
SRIRAM238 <75298072+SRIRAM238@users.noreply.github.com>
2021-01-27 12:55:51 +0530

    added buttons to start and stop music, for loop and array to display questions and reduced code length,added some extra styling

--- index1.js
+++ index1.js
@@ -1,3 +1,6 @@
+// init();
+
+// function init() {
 // constructor function to Initialize all question objects (i.e question + Multiple choice Options)
 function QuestionSet(question, option1, option2, option3, option4) {
   this.question = question;
@@ -7,68 +10,67 @@ function QuestionSet(question, option1, option2, option3, option4) {
   this.option4 = option4;
 }
 
-// list of all question objects which are to be initialized
-var ques1 = new QuestionSet("What is the capital of Sikkim?", "kohima", "Gangtok", "dispur", "itanagar");
-var ques2 = new QuestionSet("What is the capital of Arunachal Pradesh?", "Itanagar", "Hyderabad", "dispur", "Shimla");
-var ques3 = new QuestionSet("What is the capital of Manipur?", "Imphal", "Hyderabad", "amaravathi", "Bastar");
-var ques4 = new QuestionSet("What is the capital of Rajasthan?", "panaji", "Jaipur", "imphal", "Shimla");
-var ques5 = new QuestionSet("What is the capital of Mizoram?", "dispur", "chennai", "shillong", "Aizwal");
-var ques6 = new QuestionSet("What is the capital of Andhra Pradesh?", "Amaravathi", "Hyderabad", "kohima", "shimla");
-var ques7 = new QuestionSet("What is the capital of Orissa?", "itanagar", "Bhuvaneshwar", "Bhopal", "chandigarh");
-var ques8 = new QuestionSet("What is the capital of Meghalaya?", "itanagar", "Shillong", "aizwal", "dispur");
-var ques9 = new QuestionSet("What is the capital of Assam?", "mumbai", "jaipur", "chennai", "Dispur");
-var ques10 = new QuestionSet("What is the capital of Tamil Nadu?", "Chennai", "imphal", "shimla", "amaravathi");
-var ques11 = new QuestionSet("What is the capital of Kerala?", "panaji", "Trivendrum", "kohima", "ladakh");
-var ques12 = new QuestionSet("What is the capital of Goa?", "itanagar", "gandhiNagar", "chandigarh", "Panaji");
-var ques13 = new QuestionSet("What is the capital of Gujarat?", "itanagar", "shillong", "banglore", "GandhiNagar");
-var ques14 = new QuestionSet("What is the capital of Karnataka?", "Banglore", "bhopal", "chennai", "panaji");
-var ques15 = new QuestionSet("What is the capital of Chattisgarh?", "aizwal", "Raipur", "bastar", "chandigarh");
-var ques16 = new QuestionSet("What is the capital of Jharkhand?", "amaravathi", "dispur", "mumbai", "Ranchi");
-var ques17 = new QuestionSet("What is the capital of Bihar?", "itanagar", "Patna", "mumbai", "ladakh");
-var ques18 = new QuestionSet("What is the capital of Uttarakhand?", "Nainital", "Dehradun", "shillong", "gangtok");
-var ques19 = new QuestionSet("What is the capital of Maharastra?", "delhi", "bhopal", "lucknow", "Mumbai");
-var ques20 = new QuestionSet("What is the capital of punjab?", "Chandigarh", "patna", "bhuvaneshwar", "raipur");
-
-// array of all 20 question objects for random selection and others
-var quesarray = [ques1, ques2, ques3, ques4, ques5, ques6, ques7, ques8, ques9, ques10, ques11, ques12, ques13, ques14, ques15, ques16, ques17, ques18, ques19, ques20];
-var length = quesarray.length; /*to generate the random numbers*/
+var quesInfo = ["Sikkim", "Arunachal Pradesh", "Manipur", "Rajasthan", "Mizoram", "Andhra Pradesh", "Orissa", "Meghalaya", "Assam", "Tamil Nadu"];
+var option1Info = ["kohima", "Itanagar", "Imphal", "panaji", "dispur", "Amaravathi", "itanagar", "itanagar", "mumbai", "Chennai"];
+var option2Info = ["Gangtok", "Hyderabad", "Hyderabad", "Jaipur", "chennai", "Hyderabad", "Bhuvaneshwar", "Shillong", "jaipur", "imphal"];
+var option3Info = ["dispur", "dispur", "amaravathi", "imphal", "shillong", "kohima", "Bhopal", "aizwal", "chennai", "shimla"];
+var option4Info = ["itanagar", "Shimla", "Bastar", "Shimla", "Aizwal", "shimla", "chandigarh", "dispur", "Dispur", "amaravathi"];
+var quesarray = []; // array containing 10 question objects after being pushed into it
+for (var j = 0; j <= 9; j++) // looping to display a list of all question objects which are to be initialized
+{
+  var ques = new QuestionSet("What is the capital of" + " " + quesInfo[j] + "?", option1Info[j], option2Info[j], option3Info[j], option4Info[j]);
+  quesarray.push(ques);
+}
+var length = quesarray.length; /* used to generate the random numbers*/
 var randomPattern = []; /*Empty array to push all the randomly generated ques objects*/
 var userClickPattern = [];
 var finalScore = []; /*Empty array to push incremented score when answer for question is correct*/
 var k = 1; /* variable k to generate serial number for questions generated randomly */
 var start = false; /*Boolean for initial keypress event to start the quiz content*/
 var point = 0; /*variable to update the score as soon as my answer to question is correct */
-beforeGameStart();
-begin();
 var timerSound = new Audio("sounds/game sound.mp3");
-/* function call to hide the container containing question object(i.e. question and four multiple choice answers) before game start */
+timerSound.loop = true;
+beforeGameStart(); /* function call to hide the container containing question object(i.e. question and four multiple choice answers) before game start */
+gameSoundButtons();
+begin(start);
+// }
 
 
-// keypress event for the game to start
-function begin() {
-document.querySelector("#buttonclick").addEventListener("click", function() {
-  if (!start) {
-    headUpdate(); /* function call to update score after game start*/
-    quizSequence(); /*function call to generate first random question as soon as game start */
-    start = true;
-    afterGameStart(); /*function call to show the container containing question object after game starts*/
-    /* to play the background sound throught the game*/
-    // var timerSound = new Audio("sounds/game sound.mp3");
-    // timerSound.play();
 
-    gameStartSound(userClickPattern.length);  /*function call to play the background game sound*/
-
-  }
-});
-
+function gameSoundButtons() {
+  document.querySelector("#stopSound").addEventListener("click", function() {
+    timerSound.pause();
+  });
+  document.querySelector("#playSound").addEventListener("click", function() {
+    timerSound.play();
+  });
 }
 
 
+// keypress event for the game to start
+function begin(action) {
+  document.querySelector("#buttonclick").addEventListener("click", function() {
+    if (!action) {
+      headUpdate(); /* function call to update score after game start*/
+      quizSequence(); /*function call to generate first random question as soon as game start */
+      start = true;
+      afterGameStart(); /*function call to show the container containing question object after game starts*/
+      gameStartSound(userClickPattern.length); /*function call to play the background game sound*/
+
+    }
+  });
+}
+
 // function definition for title updation to increment score for every correct answer
 function headUpdate() {
-  document.querySelector("h1").innerHTML = "Your Score is" + " " + point + "/10" + ".";
-  finalScore.push(point);
-  point++;
+  document.querySelector("h1").innerHTML = "Your Score is" + " " + point + "/5" + ".";
+  if (point === 0) {
+    point++;
+  } else {
+    finalScore.push(point);
+    point++;
+  }
+
 }
 
 // function definition for randomly generated questions
@@ -91,12 +93,10 @@ function quizSequence() {
   setTimeout(function() {
     document.querySelector(".container").classList.remove("blink");
   }, 300);
-  quesarray.splice(randomNumber, 1); /* array splicing to avoid repetetion of randomly generated question objects */
-  length--;
+  // quesarray.splice(randomNumber, 1); /* array splicing to avoid repetetion of randomly generated question objects */
+  // length--;
   k++; /*serial number increment for each new question generated*/
 }
-
-
 
 // code for the click event ( i.e to click on my answer to question)
 var length1 = document.querySelectorAll(".optionclick").length;
@@ -105,9 +105,9 @@ for (var i = 0; i < length1; i++) {
     var clickContent = this.innerText;
     userClickPattern.push(clickContent);
     // condition for correct answer of each randomly generated questions
-    if ((clickContent === ques1.option2) || (clickContent === ques2.option1) || (clickContent === ques3.option1) || (clickContent === ques4.option2) || (clickContent === ques5.option4) || (clickContent === ques6.option1) || (clickContent === ques7.option2) || (clickContent === ques8.option2) || (clickContent === ques9.option4) || (clickContent === ques10.option1) || (clickContent === ques11.option2) || (clickContent === ques12.option4) || (clickContent === ques13.option4) || (clickContent === ques14.option1) || (clickContent === ques15.option2) || (clickContent === ques16.option4) || (clickContent === ques17.option2) || (clickContent === ques18.option2) || (clickContent === ques19.option4) || (clickContent === ques20.option1)) {
+    if ((clickContent === quesarray[0].option2) || (clickContent === quesarray[1].option1) || (clickContent === quesarray[2].option1) || (clickContent === quesarray[3].option2) || (clickContent === quesarray[4].option4) || (clickContent === quesarray[5].option1) || (clickContent === quesarray[6].option2) || (clickContent === quesarray[7].option2) || (clickContent === quesarray[8].option4) || (clickContent === quesarray[9].option1)) {
 
-      if (userClickPattern.length <= 9) {
+      if (userClickPattern.length <= 4) {
         headUpdate(); /*function call to update my score after answer to my question is correct*/
         correctAnswerStyle(this); /*function call to add styling when my answer is correct*/
 
@@ -116,18 +116,17 @@ for (var i = 0; i < length1; i++) {
           quizSequence();
         }, 1500);
       } else {
+        headUpdate(); /*function call to update my score after answer to my question is correct*/
         correctAnswerStyle(this);
         setTimeout(function() {
           endQuiz(); /*ending of quiz after 10th question answer is correct*/
           gameStartSound(userClickPattern.length);
         }, 1500);
-
       }
-
     }
     // else condition for wrong answers of randomly generated question
     else {
-      if (userClickPattern.length <= 9) {
+      if (userClickPattern.length <= 4) {
         wrongAnswerStyle(this); /*function call to add styling when my answer is incorrect*/
         // function call for next randomly generated question object after wrong answer for current question object
         setTimeout(function() {
@@ -139,61 +138,34 @@ for (var i = 0; i < length1; i++) {
           endQuiz(); /* ending of quiz after 10th question answer is wrong */
           gameStartSound(userClickPattern.length); /* function call to stop background game sound after quiz of 10 questions is completed*/
         }, 1500);
-
       }
     }
   });
 }
 
-
 // function definition  to display final score after the quiz of 10 questions is ended and reset all the variables to begin new game
 function endQuiz() {
-  if ( (finalScore.length > 8) && (finalScore.length <= 10) ) {
-    document.querySelector("h1").innerHTML = "Your final score is" + " " + (finalScore.length) + "/10" + "<br>" + "Congrats!!You are Perfect" + "<br>" + "click to continue";
+  if ((finalScore.length === 5)) {
+    document.querySelector("h1").innerHTML = "Your final score is" + " " + (finalScore.length) + "/5" + "<br>" + "Wow!! Perfect" + "<br>" + "click to continue";
     gameCompleteSound(1); /*function call to add sound after final score display */
     afterGameEnd(); // function call to hide the question object container after game end and reset all the stuff
-    // document.querySelector("h1").classList.add("sizing");
-    // document.querySelector(".container").classList.add("hidden");
-    // document.addEventListener("click", function() {
-    //   beforeGameStart();
-    //   document.querySelector("h1").innerHTML = "Test your IQ";
-    //   document.querySelector("#buttonclick").classList.remove("hidden1");
-    //
-    //
-    //   //   document.querySelector("h1").classList.add("sizing");
-    //   //   document.querySelector(".container").classList.add("hidden");
-    //   //   document.querySelector("#buttonclick").classList.remove("hidden1");
-    //   quesarray = quesarray.concat(randomPattern);
-    //   randomPattern = [];
-    //   userClickPattern = [];
-    //   finalScore = [];
-    //   k = 1;
-    //   point = 0;
-    //   start = false;
-    //   begin();
-    //
-    // });
-
-
-  } else if ( (finalScore.length > 5) && (finalScore.length <= 8) ) {
-    document.querySelector("h1").innerHTML = "Your final score is" + " " + (finalScore.length) + "/10" + "<br>" + "Ok!! You are Better" + "<br>" + "click to continue";
+  } else if ((finalScore.length > 3) && (finalScore.length < 5)) {
+    document.querySelector("h1").innerHTML = "Your final score is" + " " + (finalScore.length) + "/5" + "<br>" + "Ok!! Better" + "<br>" + "click to continue";
     gameCompleteSound(2);
     afterGameEnd(); // function call to hide the question object container after game end and reset all the stuff
   } else {
-    document.querySelector("h1").innerHTML = "Your final score is" + " " + (finalScore.length) + "/10" + "<br>" + "Hmmmm!! Better luck next time" + "<br>" + "click to continue";
+    document.querySelector("h1").innerHTML = "Your final score is" + " " + (finalScore.length) + "/5" + "<br>" + "Hmmmm!! Better luck " + "<br>" + "click to continue";
     gameCompleteSound(0);
     afterGameEnd(); // function call to hide the question object container after game end and reset all the stuff
   }
-
 }
-
-
 
 // function definition to hide the question object container before game start
 function beforeGameStart() {
   document.querySelector("h1").classList.add("sizing");
   document.querySelector(".container").classList.add("hidden");
   document.querySelector("#buttonnext").classList.add("buttonNextStyle");
+  document.querySelector(".playStopMusic").classList.add("playStopDisplay");
 }
 
 // function definition to show the question object container after game start
@@ -201,12 +173,13 @@ function afterGameStart() {
   document.querySelector(".container").classList.remove("hidden");
   document.querySelector("h1").classList.remove("sizing");
   document.querySelector("#buttonclick").classList.add("hidden1");
+  document.querySelector(".playStopMusic").classList.remove("playStopDisplay");
 }
 
+// function definition to start the background game sound and remove after quiz is over to display final score
 function gameStartSound(lengthRand) {
-
-  if (lengthRand === 10) {
-    timerSound.pause();      /*when game of 10 question is over and final score is displayed,the game background should stop*/
+  if (lengthRand === 5) {
+    timerSound.pause(); /*when game of 10 question is over and final score is displayed,the game background should stop*/
     timerSound.currentTime = 0;
   } else {
     timerSound.play();
@@ -218,6 +191,7 @@ function afterGameEnd() {
   document.querySelector("h1").classList.add("sizing");
   document.querySelector(".container").classList.add("hidden");
   document.querySelector("#buttonnext").classList.remove("buttonNextStyle");
+  document.querySelector(".playStopMusic").classList.add("playStopDisplay");
   document.querySelector("#buttonnext").addEventListener("click", function() {
     quesarray = quesarray.concat(randomPattern);
     randomPattern = [];
@@ -227,14 +201,13 @@ function afterGameEnd() {
     point = 0;
     headUpdate();
     quizSequence();
-    gameStartSound(userClickPattern.lenght);
+    gameStartSound(userClickPattern.length);
     document.querySelector(".container").classList.remove("hidden");
     document.querySelector("h1").classList.remove("sizing");
     document.querySelector("#buttonnext").classList.add("buttonNextStyle");
+    document.querySelector(".playStopMusic").classList.remove("playStopDisplay");
   });
 }
-
-
 
 // function definiton to add and remove class for styling after my answer is correct
 function correctAnswerStyle(content) {
@@ -290,3 +263,41 @@ function gameCompleteSound(i) {
 //       }
 //   }
 // }
+
+
+
+
+// var quesarray = [ques1, ques2, ques3, ques4, ques5, ques6, ques7, ques8, ques9, ques10, ques11, ques12, ques13, ques14, ques15, ques16, ques17, ques18, ques19, ques20];
+// var length = quesarray.length; /*to generate the random numbers*/
+
+
+
+
+
+
+/*TRIED THIS TO GO BACK TO HOME PAGE AFTER MY FINAL SCORE IS PERFECT. */
+// document.querySelector("h1").classList.add("sizing");
+// document.querySelector(".container").classList.add("hidden");
+// document.addEventListener("click", function() {
+//   beforeGameStart();
+//   document.querySelector("h1").innerHTML = "Test your IQ";
+//   document.querySelector("#buttonclick").classList.remove("hidden1");
+//
+//
+//   //   document.querySelector("h1").classList.add("sizing");
+//   //   document.querySelector(".container").classList.add("hidden");
+//   //   document.querySelector("#buttonclick").classList.remove("hidden1");
+//   quesarray = quesarray.concat(randomPattern);
+//   randomPattern = [];
+//   userClickPattern = [];
+//   finalScore = [];
+//   k = 1;
+//   point = 0;
+//   start = false;
+//   begin();
+//
+// });
+
+/* to play the background sound throught the game*/
+// var timerSound = new Audio("sounds/game sound.mp3");
+// timerSound.play();
